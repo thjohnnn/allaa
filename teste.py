807aeab12158a6bb613ee09ad398594956979457
thjohnnn <joaopedrolgp@hotmail.com>
2025-05-23 10:39:53 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -5,3 +5,5 @@ st.write("""
 # Vai tomar no cu Grupo bom
 Fabiano Dá o cu pra xandi
 """)
+
+st.image(https://www.google.com/url?sa=i&url=https%3A%2F%2Fblog.tabacariadamata.com.br%2Fcrumble-maconha%2F&psig=AOvVaw1j4RoNOc8aygfpVUNJwhfk&ust=1748093918609000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCNCC67rbuY0DFQAAAAAdAAAAABAE, use_column_width=True)
